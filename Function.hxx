2472683b71c355b46696a28beafbf853dac41483
Vivien Mallet <Vivien.Mallet@inria.fr>
2003-04-23 07:50:57 +0000

    ---

--- Function.hxx
+++ Function.hxx
@@ -49,44 +49,44 @@ namespace SeldonData
     
     //! Undefined function.
     virtual TOut operator () (T& i0)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(1)");
     }
 
     //! Undefined function.
     virtual TOut operator () (T& i0, T& i1)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(2)");
     }
 
     //! Undefined function.
     virtual TOut operator () (T& i0, T& i1,
 			      T& i2)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(3)");
     }
 
     //! Undefined function.
     virtual TOut operator () (T& i0, T& i1,
 			      T& i2, T& i3)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(4)");
     }
 
     //! Undefined function.
     virtual TOut operator () (T& i0, T& i1,
 			      T& i2, T& i3,
 			      T& i4)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(5)");
     }
 
     //! Undefined function.
     virtual TOut operator () (T& i0, T& i1,
 			      T& i2, T& i3,
 			      T& i4, T& i5)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(6)");
     }
 
     //! Undefined function.
@@ -94,8 +94,8 @@ namespace SeldonData
 			      T& i2, T& i3,
 			      T& i4, T& i5,
 			      T& i6)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(7)");
     }
 
     //! Undefined function.
@@ -103,8 +103,8 @@ namespace SeldonData
 			      T& i2, T& i3,
 			      T& i4, T& i5,
 			      T& i6, T& i7)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(8)");
     }
 
     //! Undefined function.
@@ -113,8 +113,8 @@ namespace SeldonData
 			      T& i4, T& i5,
 			      T& i6, T& i7,
 			      T& i8)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(9)");
     }
 
     //! Undefined function.
@@ -123,8 +123,8 @@ namespace SeldonData
 			      T& i4, T& i5,
 			      T& i6, T& i7,
 			      T& i8, T& i9)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(10)");
     }
 
     //! Undefined function.
@@ -134,8 +134,8 @@ namespace SeldonData
 			      T& i6, T& i7,
 			      T& i8, T& i9,
 			      T& i10)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(11)");
     }
 
     //! Undefined function.
@@ -145,8 +145,8 @@ namespace SeldonData
 			      T& i6, T& i7,
 			      T& i8, T& i9,
 			      T& i10, T& i11)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(12)");
     }
 
     //! Undefined function.
@@ -157,8 +157,8 @@ namespace SeldonData
 			      T& i8, T& i9,
 			      T& i10, T& i11,
 			      T& i12)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(13)");
     }
 
     //! Undefined function.
@@ -169,8 +169,8 @@ namespace SeldonData
 			      T& i8, T& i9,
 			      T& i10, T& i11,
 			      T& i12, T& i13)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(14)");
     }
 
     //! Undefined function.
@@ -182,8 +182,8 @@ namespace SeldonData
 			      T& i10, T& i11,
 			      T& i12, T& i13,
 			      T& i14)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(15)");
     }
 
     //! Undefined function.
@@ -195,8 +195,8 @@ namespace SeldonData
 			      T& i10, T& i11,
 			      T& i12, T& i13,
 			      T& i14, T& i15)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(16)");
     }
 
     //! Undefined function.
@@ -209,8 +209,8 @@ namespace SeldonData
 			      T& i12, T& i13,
 			      T& i14, T& i15,
 			      T& i16)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(17)");
     }
 
     //! Undefined function.
@@ -223,8 +223,8 @@ namespace SeldonData
 			      T& i12, T& i13,
 			      T& i14, T& i15,
 			      T& i16, T& i17)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(18)");
     }
 
     //! Undefined function.
@@ -238,8 +238,8 @@ namespace SeldonData
 			      T& i14, T& i15,
 			      T& i16, T& i17,
 			      T& i18)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(19)");
     }
 
     //! Undefined function.
@@ -253,8 +253,8 @@ namespace SeldonData
 			      T& i14, T& i15,
 			      T& i16, T& i17,
 			      T& i18, T& i19)
-    { 
-      throw Undefined("Function_Base<T>::operator()");
+    {
+      throw Undefined("Function_Base<T>::operator(20)");
     }
 
   };
@@ -285,29 +285,29 @@ namespace SeldonData
     
     //! Undefined function.
     virtual void operator () (T& i0)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(1)");
     }
 
     //! Undefined function.
     virtual void operator () (const T& i0, T& i1)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(2)");
     }
 
     //! Undefined function.
     virtual void operator () (const T& i0, const T& i1,
 			      T& i2, T& i3)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(4)");
     }
 
     //! Undefined function.
     virtual void operator () (const T& i0, const T& i1,
 			      const T& i2, T& i3,
 			      T& i4, T& i5)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(6)");
     }
 
     //! Undefined function.
@@ -315,8 +315,8 @@ namespace SeldonData
 			      const T& i2, const T& i3,
 			      T& i4, T& i5,
 			      T& i6, T& i7)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(8)");
     }
 
     //! Undefined function.
@@ -325,8 +325,8 @@ namespace SeldonData
 			      const T& i4, const T& i5,
 			      T& i6, T& i7,
 			      T& i8, T& i9)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(10)");
     }
 
     //! Undefined function.
@@ -336,8 +336,8 @@ namespace SeldonData
 			      T& i6, T& i7,
 			      T& i8, T& i9,
 			      T& i10, T& i11)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(12)");
     }
 
     //! Undefined function.
@@ -348,8 +348,8 @@ namespace SeldonData
 			      T& i8, T& i9,
 			      T& i10, T& i11,
 			      T& i12, T& i13)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(14)");
     }
 
     //! Undefined function.
@@ -361,8 +361,8 @@ namespace SeldonData
 			      T& i10, T& i11,
 			      T& i12, T& i13,
 			      T& i14, T& i15)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(16)");
     }
 
     //! Undefined function.
@@ -375,8 +375,8 @@ namespace SeldonData
 			      T& i12, T& i13,
 			      T& i14, T& i15,
 			      T& i16, T& i17)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(18)");
     }
 
     //! Undefined function.
@@ -390,8 +390,8 @@ namespace SeldonData
 			      T& i14, T& i15,
 			      T& i16, T& i17,
 			      T& i18, T& i19)
-    { 
-      throw Undefined("FuncCoords_Base<T>::operator()");
+    {
+      throw Undefined("FuncCoords_Base<T>::operator(20)");
     }
 
   };
