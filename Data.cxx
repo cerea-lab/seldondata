39517faf8c3c4c56fc00585c2e62ca63433cf3e7
Vivien Mallet <Vivien.Mallet@inria.fr>
2004-04-11 15:27:00 +0000

    ---

--- Data.cxx
+++ Data.cxx
@@ -3563,7 +3563,7 @@ namespace SeldonData
     int NbElements = data_.numElements();
     
     for (int i=0; i<NbElements; i++)
-      function.Apply(data[i]);
+      function(data[i]);
   }
 
   //! Applies a given function on all elements.
